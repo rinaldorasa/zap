fefd4d383d9c8d46faaf1cf72714fef2734870c3
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2020-12-06 13:00:31 +0100

    Add files via upload

---
+++ piedipagina_menu.js
@@ -0,0 +1,50 @@
+
+/*  piedipagina menu 20201206 1015  */
+
+
+document.getElementById("piedipagina_menu").innerHTML= ' <section  '+ 	'  class="position-fixed '+ 	'    bottom-0 border border-warning"   '+ 	'   style=" width:100% !important;  height:auto !important; text-align:left !important;  background-color:rgba(255,170,0,0.65); ">  '+ 	'    '+ 	'    '+ 	
+
+' <span style="margin-right:2.3rem !important;  padding:0.2rem; border-radius:0.4rem !important;  border:solid 0.05rem navy !important;">   '+ 	'    '+ 	
+
+' <button type="button" class="btn shadow-none"   '+ 	'    data-toggle="collapse" data-target="#collapsemenuapdp"   '+ 	'    aria-expanded="false"  '+ 	'  style="border-radius:0rem !important; border:solid 0rem black; "> '+ 	'    '+ 	'  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-back" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2z"/> '+ 	'  </svg>  '+ 	'    '+ 	
+' </button>      '+	 	
+
+
+'  <span style="visibility:hidden !important;">menu</span>  '+ 	'    <a href="#fine">  '+ 	'   <svg xmlns="http://www.w3.org/2000/svg"   height="24" viewBox="0 0 24 24"  width="24"><path d="M0 0h24v24H0V0z"   fill="none" />  <path fill="maroon"   d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/> '+ 	'   </svg> </a>   '+ 	'  <span style="visibility:hidden !important;">menu</span>  '+ 	'    '+ 	'   <a href="#">  '+ 	'   <svg xmlns="http://www.w3.org/2000/svg"   height="24"   viewBox="0 0 24 24"   width="24">  <path d="M0 0h24v24H0V0z"   fill="none"/>  <path fill="maroon"   d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/>  '+ 	'  </svg> </a>    '+ 	'    '+	 	' </span>   '+ 	
+'    '+ 	'    '+ 	'    '+ 	'    '+ 	
+
+
+' <span>  '+ 	'    '+ 	
+
+
+
+'  <div id="collapsemenuapdp" class="collapse dropdown-menuY  border-0"   '+ 	'    style="    padding:0rem !important;  '+ 	'   '+ 	
+'     background-color:rgba(0,85,85,0);   '+ 	'    border-left:solid 0.1rem rgba(255,170,0,1) !important;   '+ 	
+'   border-radius:0rem !important;  '+ 	'     background-color:rgba(255,170,0,0); ">     '+ 
+
+
+
+
+
+
+' <ul style="border:solid 0.5rem indigo !important;  height:11.5rem !important; overflow:auto !important; border-radius:0.2rem !important; ">'+
+
+
+	
+' 	<a class="btn-warning rounded"   '+ '  href="http://beatsupernovarasa.altervista.org/index.html">  '+ 	'  	<span>home_page</span></a>  '+ 	
+
+
+
+
+
+' 	 '+ 	'     '+  	' </ul>  '+ 	
+
+' </div> '+
+' </span> '+ 	
+
+' <span style="font-size:0.5rem !important;">   vers. 20201206_1015   </span>    '+ 
+
+' </section>  ';
+
+
+
