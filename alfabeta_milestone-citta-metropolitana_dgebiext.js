997198e91c498439b8891b59a08edbf34f7d9409
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-01-25 18:45:10 +0100

    Add files via upload

---
+++ alfabeta_milestone-citta-metropolitana_dgebiext.js
@@ -0,0 +1,299 @@
+document.getElementById("alfabeta_milestone-citta-metropolitana_dgebiext").innerHTML=
+'<link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet"> '+
+''+
+''+
+'<div class="x" style=" '+
+' position:fixed !important; '+
+' bottom:0rem !important; '+
+' left:0rem !important; '+
+''+
+' margin-left:0.5% !important; '+
+' margin-right:0.5% !important; '+
+''+
+' background-color:oldlace !important; '+
+' background-color:rgba(253,245,230,0.95) !important; '+
+''+
+''+
+' width:99% !important; '+
+' height:auto !important; '+
+' font-size:1.1rem !important; '+
+' '+
+' '+
+' text-align:left !important; '+
+' '+
+' border:solid 0.2rem transparent !important; '+
+' '+
+' '+
+' "> '+
+''+
+''+
+''+
+'<button type="button" '+
+'class="btn shadow-none bg-warning rounded position-fixed" '+
+'data-toggle="collapse"'+
+'data-target="#COLLAPSE_MENU"'+
+'aria-expanded="false"'+
+'style="top:2.5rem;right:0;font-size:1rem!important;">'+
+'<svg width="1rem" height="1rem" viewBox="0 0 16 16" class="bi bi-back" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2z"/></svg>'+
+'</button>'+
+''+
+''+
+''+
+''+
+'<span style="position:fixed;top:50%;margin-top:-3rem!important;right:0;background-color:oldlace!important;">'+
+'<a style="position:static!important;top:17rem!important;right:0;display:inline-block;" href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="maroon" d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg></a>'+
+'<br/>'+
+'<a style="position:static!important;top:11rem!important;right:0;display:inline-block;" href="#fine"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none" /><path fill="maroon" d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/></svg></a> '+
+'</span>'+
+''+
+' '+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+'<div '+
+' id="COLLAPSE_MENU" '+
+' class="collapse dropdown-menuY border-0" '+
+' style=" '+
+' margin-left:2.41rem !important; '+
+' margin-right:2.41rem !important; '+
+' margin-bottom:2rem !important; '+
+' border:solid 0.2rem transparent !important;"> '+
+''+
+' '+
+''+
+'<blockquote style=" '+
+' border:solid 0.25rem indigo !important; '+
+' border:solid 0.10rem rgba(75,0,130,1) !important; '+
+' height:11.5rem !important; '+
+' overflow:auto !important; '+
+' border-radius:0.2rem !important; "> '+
+''+
+'<a class="btn rounded" '+
+' href="#argomento-0000"> '+
+'<span class="rounded p-1 fw-bold" '+
+' style=" '+
+' background-color:indigo !important; '+
+' color:oldlace !important; '+
+' font-size:small !important; '+
+' border:solid 0.1rem indigo !important; "> '+
+' argomento-0000'+
+'</span></a> '+
+''+
+'<a class="btn rounded" '+
+' href="#argomento-0001"> '+
+'<span class="rounded p-1 fw-bold" '+
+' style=" '+
+' background-color:indigo !important; '+
+' color:oldlace !important; '+
+' font-size:small !important; '+
+' border:solid 0.1rem indigo !important; "> '+
+' argomento-0001'+
+'</span></a> '+
+''+
+''+
+'<a class="btn rounded" '+
+' href="#argomento-0002"> '+
+'<span class="rounded p-1 fw-bold" '+
+' style=" '+
+' background-color:indigo !important; '+
+' color:oldlace !important; '+
+' font-size:small !important; '+
+' border:solid 0.1rem indigo !important; "> '+
+' argomento-0002'+
+'</span></a> '+
+''+
+''+
+' '+
+' '+
+'</blockquote> '+
+''+
+' '+
+'</div> '+
+'</div> '+
+' '+
+''+
+''+
+''+
+''+
+'<span  class="offy" onclick="SEMAFOROverde()" '+
+'style="position:fixed;bottom:2.5rem;right:0;background-color:oldlace;">'+
+'<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="teal" class="bi bi-toggle-on" viewBox="0 0 16 16">'+
+'<path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>'+
+'</svg>'+
+'</span>'+
+''+
+'<span  class="offx" onclick="SEMAFOROrosso()" '+
+'style="position:fixed;bottom:2.5rem;right:0;background-color:oldlace;">'+
+'<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="maroon" class="bi bi-toggle-off" viewBox="0 0 16 16">'+
+'<path d="M11 4a4 4 0 0 1 0 8H8a4.992 4.992 0 0 0 2-4 4.992 4.992 0 0 0-2-4h3zm-6 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM0 8a5 5 0 0 0 5 5h6a5 5 0 0 0 0-10H5a5 5 0 0 0-5 5z"/>'+
+'</svg>'+
+'</span>'+
+''+
+''+
+''+
+''+
+''+
+'<style>'+
+'/* tablone stile */'+
+'/* '+
+'20210122 0945'+
+''+
+'*/'+
+''+
+''+
+':root {'+
+'--bsnr-giallo: rgba(255, 170, 0, 0.035);'+
+'--bsnr-html: rgba(255, 170, 0, 0.035);'+
+'--bsnr-oldlace: rgba(253,245,230,0.5);'+
+'--bsnr-oldlace: rgba(253,245,230,0.275);'+
+''+
+'background-color:rgba(255,170,0,0.5);'+
+''+
+'letter-spacing:0.025rem !important;'+
+''+
+'}'+
+''+
+''+
+'html * {'+
+'color:black !important;'+
+'font-size:1.2rem !important;'+
+'font-family: "PT Mono" , monospace !important;'+
+'text-align:left !important;'+
+'} '+
+''+
+'body{'+
+'background-color:oldlace;'+
+'background-color:rgba(253,245,230,0.5);'+
+''+
+'}'+
+''+
+'main{'+
+'margin-top:2.5rem !important;'+
+'margin-bottom:18.5rem !important;'+
+'border:solid 0.1rem rgba(255,0,0,0.15) !important;'+
+'/* overflow:auto !important; */'+
+''+
+'}'+
+''+
+''+
+'table#tablone, table#tablette{ '+
+'table-layout:fixed !important;'+
+'width:100% !important;'+
+'border:solid 0.0rem green !important;'+
+'}'+
+''+
+'td#tablone-td-sinistra{'+
+'width:0.5rem !important;'+
+'border:solid 0.0rem grey !important;'+
+'}'+
+''+
+'td#tablone-td-destra{'+
+'border:solid 0.0rem lawngreen !important;'+
+'}'+
+''+
+''+
+''+
+'aside{overflow:auto !important;}'+
+'aside a{'+
+'padding-left:0.2rem !important;'+
+'padding-right:0.2rem !important;'+
+'background-color:var(--bs-blue) !important;'+
+'color:var(--bs-white) !important;'+
+'font-size:1rem !important;'+
+'box-shadow:0.1rem 0.1rem 0.2rem black !important;'+
+'border-radius:0.2rem !important;'+
+'}'+
+'aside.argomento {'+
+'margin-top:1.25rem !important;'+
+'margin-bottom:1.25rem !important;'+
+'font-size:1rem !important;'+
+'text-align: justify !important;'+
+'text-justify: inter-word !important;'+
+'overflow:auto !important;'+
+'display:block !important;'+
+'}'+
+''+
+'aside.argomento#argomento-0000 {'+
+'/* TITOLO */'+
+'font-size:1.75rem !important;'+
+'font-weight: bold !important;'+
+'}'+
+'aside.argomento#argomento-0001,'+
+'aside.argomento#argomento-0002,'+
+'aside.argomento#argomento-0003'+
+'{'+
+'/* TITOLETTO */'+
+'font-size:1.45rem !important;'+
+'font-weight: bold !important;'+
+'}'+
+'aside.argomento#argomento-0004, '+
+'aside.argomento#argomento-0005'+
+'{'+
+'/* CODICE HTML TESTO */'+
+'background-color: bisque !important;'+
+'color:black !important;'+
+'font-size:1.0rem !important;'+
+'font-weight: bold !important;'+
+'}'+
+'aside.argomento pre{'+
+'/* CODICE HTML PRE */'+
+'background-color:oldlace !important;'+
+'color:black !important;'+
+'font-size:0.85rem !important;'+
+'}'+
+'span.id{'+
+'background-color: beige !important;'+
+'color:gray !important;'+
+'font-size:0.5rem !important;'+
+'display:inline !important;'+
+'}'+
+''+
+''+
+''+
+'.note{'+
+'padding-left:0.2rem !important;'+
+'padding-right:0.2rem !important;'+
+'background-color: beige !important;'+
+'color:black !important;'+
+'font-size:0.95rem !important;'+
+'border-radius:0.2rem !important;'+
+'border:solid 0.1rem var(--bs-indigo) !important;'+
+'}'+
+'.span-block{display:block !important;'+
+'}'+
+'.span-block-fine{display:inline !important;'+
+'}'+
+'</style>'+
+''+
+''+
+'';
+
+
+
+function SEMAFOROrosso(){
+elemento=document.querySelector(".x");
+elemento.style.visibility="hidden";
+elemento=document.querySelector(".offx");
+elemento.style.visibility="hidden";
+elemento=document.querySelector(".offy");
+elemento.style.visibility="visible";
+
+}
+
+function SEMAFOROverde(){
+elemento=document.querySelector(".x");
+elemento.style.visibility="visible";
+elemento=document.querySelector(".offy");
+elemento.style.visibility="hidden";
+elemento=document.querySelector(".offx");
+elemento.style.visibility="visible";
+
+}
+
+
---
+++ dirview.php
@@ -0,0 +1,348 @@
+<!doctype html>
+<html lang="it">
+
+<head>
+
+<meta http-equiv="content-type"
+content="text/html; charset=UTF-8">
+<meta name="viewport"
+content="width=device-width, initial-scale=1,
+shrink-to-fit=no">
+<meta name="generator"
+content="AlterVista - Editor HTML">
+<title> </title>
+
+
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+crossorigin="anonymous"> </script>
+
+<link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js">
+</script>
+
+<style>
+body{
+background-color:oldlace;
+}
+</style>
+
+
+<style>
+
+
+body, main, section, article, div, p,
+span, header, footer { font-size:1em; overflow:visible !important; }
+
+main{ 
+margin-top:4.5em; margin-bottom:0.0em;
+margin-left:3em; margin-right:3em;
+border-top:solid 0em maroon;
+border-bottom:solid 0em maroon;
+border-left:solid 0.2em maroon !important;
+border-right:solid 0.2em maroon !important;
+padding:0.4em;
+background-color:rgba(222,184,135,0.25); /* burlywood */
+color:black; 
+}
+
+
+section{
+margin-left:4em !important;margin-right:4em !important;
+margin-top:0em; margin-bottom:0em;
+font-family:monospace;}
+
+header, footer{ margin-top:4.5em; }
+
+img{ width:100%;}
+
+
+div.testo{
+text-align:left; max-height:9em;
+overflow:auto !important; }
+
+div.immagine{
+text-align:left; max-height:9em;
+overflow:auto !important; }
+
+div.nerobianco{background-color:black; color:white;}
+
+
+
+.centro{ text-align:center !important;
+margin:auto !important; padding:0 !important; }
+
+.sfondo{
+background-image:url(https://ed-ora-andiamo.com/immagini/carta01x.gif);
+background-repeat: repeat;
+background-attachment: fixed;
+background-color:rgba(253,245,230,0.5); /* old lace */
+}
+
+.bordofooter{border:solid 1px rgba(0,0,0,0);}
+
+
+
+
+/*  debugging  */
+.bordo{border:solid 0px lime !important;}
+
+/*  
+.bordo000{border:solid 2px black !important;}
+
+.bordo111{border:solid 2px red !important;}
+.bordo121{border:solid 2px orange !important;}
+.bordo131{border:solid 2px green !important;}
+  */
+
+/*
+ .bordo4{font-size:1.0em;
+ border:solid 0.02em;
+ font-weight:normal !important;
+ gray;text-align:left;
+ padding:0.4em;
+ overflow:auto !important;}
+ */
+
+p#code{background-color:burlywood;}
+p#effe{background-color:orange;}
+
+
+
+</style>
+
+
+
+<style>
+
+section#sedicix1{display:none !important;}
+section#ottox2{display:none !important;}
+section#quattrox4{display:none !important;}
+
+
+ .bordo4{font-size:1.0em;
+ border:solid 0.02em;
+ font-weight:normal !important;
+ gray;text-align:left;
+ padding:0.4em;
+ overflow:auto !important;}
+</style>
+
+</head>
+
+<body onload="AttendereLoading()"  
+class="sfondo" style="background-color:gray;">
+
+
+<section class="ui grid container centro bordo2">
+	<article class="sixteen wide left aligned column bordo3">
+		<div class="bordo4" style="visibility:hidden;">
+		interlinea
+		</div>
+	</article>	
+</section>
+
+
+<main class="bordo1" style="">
+
+
+<section id="indice00" class="ui grid container centro bordo2"
+style="position:static; left:3em;top:4em;">
+	<article class="sixteen wide left aligned column bordo3">
+		<div class="nerobianco bordo4">
+		<p style="display:none;">
+		<i class=""></i>
+		<span style="color:lightgrey;"></span><br>
+		</p>
+		<p style="display:none;"></p>
+				
+		<span style="visibility:show;">
+		<a href="#"
+		 style="position:fixed; right:0;top:30%;
+		 border:solid 0.02em black;
+		 border-radius:0.2em;
+		 background-color:maroon;">
+		<i class="chevron circle up  icon">
+		</i>
+		</a>
+		<a href="#fine"
+		 style="position:fixed; right:0;top:35%;
+		 border:solid 0.02em black;
+		 border-radius:0.2em;
+		 background-color:maroon;">
+		<i class="chevron circle down icon">
+		</i>
+		</a>
+		
+		</span>
+		
+		<p>
+			
+		<style>
+		.cursore {
+		  background: lime;
+		  line-height: 17px;
+		  margin-left: 3px;
+		  -webkit-animation: blink 0.8s infinite;
+		  width: 7px;
+		  height: 15px;
+		}
+		@-webkit-keyframes blink {
+		  0% {background: #222}
+		  50% {background: lime}
+		  100% {background: #222}
+		}
+		</style>
+		<table>
+		<tr>
+		<td>
+		<span style="">
+		<div  style="margin:0;padding:0;" class="cursore"></div>
+		</span><td><i class="map icon"></i></td>
+		</li></ul>
+		</td>
+		<td>
+		</td></tr>
+		</table>
+		
+		
+
+
+
+<section id="directory" class="ui grid container centro bordo2">
+	<article class="sixteen wide left aligned column bordo3">
+		<div class="bordo4"
+		style="
+		background-color:black;
+		color:white;
+		">
+		<p>
+		<i class="folder outline icon"></i> 
+		<span style="font-size:1.5em;">Directory</span>
+		</p>
+		
+		<?php
+		$dir    = './';
+		$files1 = scandir($dir);
+		// $files2 = scandir($dir, 1);
+
+		print_r($files1);
+
+		// print_r($files2);
+		?>
+		
+		</div>
+	</article>
+</section>	
+
+
+
+<section id="contafiles" class="ui grid container centro bordo2">
+	<article class="sixteen wide left aligned column bordo3">
+		<div class="bordo4"
+		style="
+		background-color:black;
+		color:white;
+		">
+		<p>
+		<i class="folder outline icon"></i> 
+		<span style="font-size:1.5em;">
+		Numero files: 
+		</span>
+		</p>
+		
+		<!--
+		< ?php
+		$dir    = './';
+		$files1 = scandir($dir);
+		// $files2 = scandir($dir, 1);
+
+		print_r($files1);
+
+		// print_r($files2);
+		? >
+		-->
+		
+		
+		
+		<?php 
+  
+		$directory = getcwd()."/"; 
+		  
+		$filecount = 0; 
+		  
+		$files2 = glob( $directory ."*" ); 
+		  
+		if( $files2 ) { 
+			$filecount = count($files2); 
+		} 
+		  
+		echo $filecount . " files "; 
+		  
+		?> 
+
+		
+		</div>
+	</article>
+</section>	
+
+
+
+
+
+
+</main> 
+
+
+
+<blockquote style="margin-top:2rem; 
+padding:0;
+border:solid 0.2rem rgba(250,242,221,0);  text-align:center;">
+
+ <div id="crediti" 
+ style=" text-align:center !important;">
+ <span style="color:green; font-size:large;"></span>
+ </div>
+ 
+ 
+ <script type="application/javascript" src="./t101/crediti.js"></script> 
+ 
+</blockquote> 
+
+ 
+  
+<div id="fine"></div>  
+</body>
+
+
+
+</html>
+
+<!--
+< ?php
+$actual_link = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+
+$oggi_est = date("l") . " ";
+$oggi_est = $oggi_est . date("Y/m/d");
+$oggi_est_time = date("h:i:sa");
+
+$fp = fopen('/membri/edoraandiamo/text.txt', 'w');
+fwrite($fp, $actual_link. " " .$oggi_est. " ". $oggi_est_time);
+fclose($fp);
+
+$myfile = fopen("/membri/edoraandiamo/logs.txt", "a") or die("Unable to open file!");
+// $actual_link = "cronologia";
+fwrite($myfile, "\n". $actual_link. " " .$oggi_est. " ". $oggi_est_time);
+fclose($myfile);
+
+? >
+
+
+-->
+
