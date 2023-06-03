aaa0f5a41a54785e8f8b01d3faafaabfe8d661f4
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2023-06-03 08:29:52 +0200

    Add files via upload

---
+++ THE_BRIDGE_php_23603/THE_BRIDGE_23601_1430b.php
@@ -0,0 +1,864 @@
+<!doctype html>
+<html id="HTML__" lang="it">
+
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="AlterVista - Editor HTML"/>
+
+
+
+ 
+
+
+
+<!-- THE_BRIDGE_23601_1300a -->
+<title>
+?
+*$$ T
+
+THE_BRIDGE_23601_1300a.html
+
+
+</title>
+<link rel="icon" type="image/png" href="https://it.altervista.org/images/favicon/favicon-16x16.png" sizes="16x16"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+
+<style>
+html * {font-size:auto;font-family:monospace;}
+
+
+body{border:dotted 0.15rem rgba(255,0,0,0.0);}
+
+
+q{border-bottom:dashed 0.15rem orange;}
+
+
+ol { list-style: none; counter-reset: ColoreCounter__;}
+ol li { counter-increment: ColoreCounter__;}
+ol li::before { content: counter(ColoreCounter__) ". "; background-color:aqua;color: black;font-size:75%;border:solid 0.15rem aqua;border-radius: 100%;display:inline-block;}
+
+
+</style>
+
+
+<style>
+/* UTILITIES ini */
+@keyframes spin{ 0%{transform:rotate(0deg);} 100% {transform:rotate(360deg);}}
+
+.spinner{width:48px;height:48px; width:2rem;height:2rem; border:solid 4px red; border:solid 0.25rem red; border:solid 0.25rem red; border:solid 0.25rem red; border-top:solid 4px black; border-top:solid 0.25rem transparent; background-color:transparent; border-radius:50%; overflow:none; animation: spin 1.5s linear infinite;}
+.spinmin
+{
+width: 1.00rem;
+height:1.00rem; 
+border:solid 0.175rem orange; 
+border-top:solid 0.175rem transparent; 
+background-color:transparent; 
+border-radius:50%; 
+display:inline-block;
+animation: spin 1.5s linear infinite;
+}
+
+/* UTILITIES fin */ 
+
+</style>
+
+<script>
+// rilevazione dispositivo
+//
+//
+
+DESKTOP_CHECK = 1 ;
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+{ DESKTOP_CHECK = 0 ; } else { DESKTOP_CHECK = 1 ; }
+
+HeightHeader__=10;
+HeightHeader__=15;
+
+
+
+DimensionePillolaCopy__=1.25;
+
+if(DESKTOP_CHECK === 0 ){}
+if(DESKTOP_CHECK === 1 ){
+HeightHeader__=15;
+DimensionePillolaCopy__=0.50; 
+
+}
+
+
+
+
+
+// rilevazione width height
+// i valori possono variare
+// a seconda del tipo di
+// dispositivo
+// 
+function LarghezzaRem__(){let Larghezza__=window.innerWidth/16;return Larghezza__;}
+function AltezzaRem__(){let Altezza__=window.innerHeight/16;return Altezza__;}
+
+Larghezza__=LarghezzaRem__();
+Altezza__=AltezzaRem__();
+
+
+
+
+FattMargin__=1;
+FattPadding__=1;
+
+MarginHeaderTop__=0;
+MarginHeaderRight__=0;
+MarginHeaderBottom__=0;
+MarginHeaderLeft__=0;
+
+PaddingHeaderTop__=0;
+PaddingHeaderRight__=1;
+PaddingHeaderBottom__=0;
+PaddingHeaderLeft__=1;
+
+
+HeightHeader__=HeightHeader__+(MarginHeaderTop__ * FattMargin__)+(MarginHeaderBottom__ * FattMargin__)+(PaddingHeaderTop__ * FattPadding__)+(PaddingHeaderBottom__ * FattPadding__);
+
+
+FonteDinamica__=2.5;
+
+
+
+FonteSizeXChiudiHeader__=2.50;
+FonteSizeXChiudiHeader__=3.25;
+FonteSizeXQuestionMarkPopUp__=FonteSizeXChiudiHeader__;
+LarghezzaXQuestionMarkPopUp__=(Larghezza__*1)-(FonteSizeXChiudiHeader__*6);
+AltezzaXQuestionMarkPopUp__=(Altezza__*1.00)-(HeightHeader__*0.50);
+AltezzaXQuestionMarkPopUp__=(Altezza__*1.00)-(HeightHeader__*0.75);
+AltezzaXQuestionMarkPopUp__=(Altezza__*1.00)-(HeightHeader__*0.95);
+AltezzaXQuestionMarkPopUp__=(Altezza__*1.00)-(HeightHeader__*1.00);
+
+
+</script>
+
+</head>
+
+<body id="BODY__">
+
+
+
+
+<section>
+<button id="APRI_HEADER__" onclick="ApriHeader()" >X</button>
+</section>
+
+
+
+<header id="HEADER__">?HEADER__</header>
+<section id="SETTAGGI__"></section>
+<script>
+document.getElementById("HEADER__").innerHTML=
+`
+<style>
+
+#HEADER__{
+position:fixed;
+top:0rem;
+top:`+0+`rem;
+left:0rem;
+
+margin-top:`+MarginHeaderTop__+`rem;
+margin-right:`+MarginHeaderRight__+`rem;
+margin-bottom:`+MarginHeaderBottom__+`rem;
+margin-left:`+MarginHeaderLeft__+`rem;
+
+padding-top:`+PaddingHeaderTop__+`rem;
+padding-right:`+PaddingHeaderRight__+`rem;
+padding-bottom:`+PaddingHeaderBottom__+`rem;
+padding-left:`+PaddingHeaderLeft__+`rem;
+
+
+
+
+background-color:oldlace;
+background-color:rgba(200,200,190,0.85);
+background-color:rgba(190,190,200,0.85);
+background-color:rgba(190,190,200,0.95);
+
+color:black;
+width:100%;
+
+
+height:`+(HeightHeader__ * 1)+`rem;
+overflow:auto;
+
+z-index:1000000000!important;
+
+}
+
+
+#CHIUDI_HEADER__{
+position:absolute;top:0rem;right:`+(PaddingHeaderRight__*2)+`rem;text-align:right;
+padding-left:`+(FonteSizeXChiudiHeader__*1)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*1)+`rem;
+
+background-color:red;color:white;
+font-size:2.5rem;
+font-size:`+(FonteSizeXChiudiHeader__*1)+`rem;
+border-radius:0.5rem;
+cursor:pointer;
+}
+#APRI_HEADER__{
+position:absolute;top:0rem;right:0rem;text-align:right;
+/* position:fixed;top:0rem;right:0rem;text-align:right; */
+
+background-color:green;color:white;
+font-size:2.5rem;
+font-size:`+(FonteSizeXChiudiHeader__*1)+`rem;
+text-decoration:line-through;
+border-radius:0.5rem;
+cursor:pointer;
+z-index:1000000000!important;
+}
+
+
+
+
+
+
+#CONTENITORE_COPY_SI_NO__{
+position:absolute;top:37.5%;right:2.50rem;
+position:absolute;top:35.0%;right:2.50rem;
+position:absolute;top:32.5%;right:2.50rem;
+position:absolute;top:32.5%;right:2.25rem;
+position:absolute;top:32.5%;right:4.75rem;
+position:absolute;top:32.50%;right:2.75rem;
+position:absolute;top:32.25%;right:2.75rem; /* mobile */
+
+
+padding-right:`+(FonteSizeXChiudiHeader__*1)+`rem;
+width:`+(FonteSizeXChiudiHeader__*0.725)+`rem;
+width:`+(FonteSizeXChiudiHeader__*1.250)+`rem;
+width:`+(FonteSizeXChiudiHeader__*1.450)+`rem; /* mobile */
+
+
+height:`+(FonteSizeXChiudiHeader__*1.0)+`rem;
+
+text-align:right;
+border:solid 0.00rem navy;
+border:solid 0.00rem navy;
+
+display:inline-block;
+}
+
+#COPY_SI__{
+padding-left:`+(FonteSizeXChiudiHeader__*1)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*1)+`rem;
+background-color:green!important;color:white!important;
+font-size:`+(FonteDinamica__*1)+`rem; 
+font-size:2.5rem;
+border-radius:0.50rem;
+cursor:pointer;
+display:block;}
+#COPY_NO__{
+padding-left:`+(FonteSizeXChiudiHeader__*1)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*1)+`rem;
+background-color:red!important;color:white!important; 
+font-size:`+(FonteDinamica__*1)+`rem;
+font-size:2.5rem;
+text-decoration:line-through;
+border-radius:0.50rem;
+cursor:pointer;
+display:none;}
+
+#PILLOLA_COPY__{
+padding-left:`+(FonteSizeXChiudiHeader__*1.12)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*1.12)+`rem;
+padding-left:`+(FonteSizeXChiudiHeader__*0.25)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*0.25)+`rem;
+padding-left:`+(FonteSizeXChiudiHeader__*0.00)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*0.00)+`rem;
+
+padding-left:`+(FonteSizeXChiudiHeader__*0.00)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*0.00)+`rem;
+
+
+background-color:aqua;
+
+font-size:`+(DimensionePillolaCopy__*1)+`rem;
+font-size:`+(DimensionePillolaCopy__*0.5)+`rem;
+font-size:`+(DimensionePillolaCopy__*1.5)+`rem;
+
+
+
+
+vertical-align:super;
+border:solid 0.00rem aqua;
+border:solid 0.05rem white;
+
+border-radius:0%;
+border-radius:200%;
+
+
+}
+
+
+
+
+
+
+
+#QUESTION_MARK_POPUP__{
+position:absolute;bottom:0rem;right:`+(PaddingHeaderRight__*2)+`rem;text-align:right;
+padding-left:`+(FonteSizeXChiudiHeader__*1)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*1)+`rem;
+background-color:teal;color:white;
+font-size:2.5rem;
+font-size:`+(FonteSizeXQuestionMarkPopUp__*1)+`rem;
+border-radius:0.5rem;
+cursor:pointer;
+}
+#ALT_QUESTION_MARK_POPUP__{
+position:absolute;bottom:0rem;right:`+(PaddingHeaderRight__*2)+`rem;text-align:right;
+padding-left:`+(FonteSizeXChiudiHeader__*1)+`rem;
+padding-right:`+(FonteSizeXChiudiHeader__*1)+`rem;
+background-color:purple;color:white;
+font-size:2.5rem;
+font-size:`+(FonteSizeXQuestionMarkPopUp__*1)+`rem;
+border-radius:0.5rem;
+cursor:pointer;
+}
+
+
+
+#VALORE_di_FonteDinamica__{visibility:hidden;}
+#VALORE_di_FonteDinamica__>div,
+#FORM_per_inserimento_nome_file__>div,
+#LIV_01__
+{
+display:block;
+}
+
+#VALORE_di_FonteDinamica__>q,
+#FORM_per_inserimento_nome_file__>q,
+#LIV_01__>q
+{
+font-size:1rem;
+}
+
+
+
+#INPUT_INSERITO__{
+visibility:hidden;
+}
+
+
+#RANGER__{
+width:100%;
+width:75%;
+
+}
+
+
+
+
+
+#MAIN__{
+margin-top:`+(HeightHeader__ * 1)+`rem;
+margin-top:`+((HeightHeader__ * 1) + MarginHeaderTop__ + MarginHeaderBottom__ + PaddingHeaderTop__ + PaddingHeaderBottom__)+`rem;
+
+margin-bottom:`+(HeightHeader__ * 1)+`rem;
+margin-bottom:`+((HeightHeader__ * 1) + MarginHeaderTop__ + MarginHeaderBottom__ + PaddingHeaderTop__ + PaddingHeaderBottom__)+`rem;
+
+}
+
+
+
+
+</style>
+
+
+<button id="CHIUDI_HEADER__" onclick="ChiudiHeader()" >X</button> 
+
+
+<article id="CONTENITORE_COPY_SI_NO__">
+<span id="PILLOLA_COPY__">&nbsp;&nbsp;</span>
+<button id="COPY_SI__" onclick="COPY_SI()">&#35;</button>
+<button id="COPY_NO__" onclick="COPY_NO()">&#35;</button>
+</article>
+
+<button id="ALT_QUESTION_MARK_POPUP__" onclick="ChiudiQuestionMarkPopup()" >x</button> 
+<button id="QUESTION_MARK_POPUP__" onclick="QuestionMarkPopup()" >?</button> 
+
+<div id="POPUP__" class="animate__animated animate__backInUp">
+<div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">POPUP__ Attendere...</q>
+</div>
+
+
+<div id="VALORE_di_FonteDinamica__">
+<div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">VALORE_di_FonteDinamica__ Attendere...</q>
+</div> 
+
+<div id="FORM_per_inserimento_nome_file__">
+<div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">FORM_per_inserimento_nome_file__ Attendere...</q>
+</div>
+
+
+<div id="LIV_01__">
+<div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">LIV_01__ Attendere...</q>
+</div>
+
+
+
+
+`;
+
+
+document.getElementById("POPUP__").innerHTML=
+`
+<style>
+#POPUP__{
+
+position:fixed;bottom:0%;
+left:50%;
+left:0rem;
+
+width:`+(LarghezzaXQuestionMarkPopUp__*1)+`rem;
+margin-left:-`+(LarghezzaXQuestionMarkPopUp__*0.5)+`rem;
+
+width:100%;
+margin:0rem;
+
+height:`+(AltezzaXQuestionMarkPopUp__*1)+`rem;
+
+font-size:`+(FonteDinamica__*1)+`rem;
+
+background-color:lime;
+background-color:rgb(195,215,245);
+
+
+color:black;
+border-top:solid 0.15rem rgb(85,25,225);
+
+display:none;
+overflow:auto;
+}
+
+
+#CONTENITORE_CHIUDI_QUESTION_MARK_POPUP__{
+margin:0rem;padding:0rem;
+background-color:transparent;
+text-align:center;
+border-bottom:solid 0.15rem purple;
+}
+
+
+#CHIUDI_QUESTION_MARK_POPUP__{
+display:inline-block;
+display:block;
+
+cursor:pointer;
+}
+/* #CHIUDI_QUESTION_MARK_POPUP__:hover{background-color:purple;color:white;border-radius:0.50rem;} */
+
+#CHIUDI_QUESTION_MARK_POPUP__ span{
+background-color:transparent;color:purple;border-radius:0.00rem;word-break:break-all;
+}
+#CHIUDI_QUESTION_MARK_POPUP__ span:hover{
+background-color:transparent;color:olive;border-radius:0.00rem;
+
+
+
+
+
+
+
+#CONTENITORE_DI_POPUP__{
+position:static;
+
+}
+#CONTENUTO_DI_POPUP__{
+position:static;
+}
+
+</style>
+
+<div id="CONTENITORE_DI_POPUP__">
+<div id="CONTENUTO_DI_POPUP__">
+
+
+<article id="CONTENITORE_CHIUDI_QUESTION_MARK_POPUP__">
+<blockquote id="CHIUDI_QUESTION_MARK_POPUP__" onclick="ChiudiQuestionMarkPopup()">
+<span>TheRollingThunderProject released THE_BRIDGE_23601_1300a</span>
+</blockquote>
+</article>
+
+<ol>
+<li><div id="CHECK_STOP_AND_GO__"></div></li>
+
+</ol>
+
+<blockquote id="COMMENTI__">
+
+
+<q>
+<ol>
+<li><pre>word-break:break-all</pre></li>
+<li>Isolare pseudo-moduli wrapping con section (working progress)</li>
+<li>Ogni elemento ha un riferimento dgebi</li>
+<li>Step02 ha il popup</li>
+<li>La funzione copy è essenziale per il programma</li>
+<li>Il <q>debugging</q> è eseguito per individuare alcune anomalie nelle operazioni di stop and go</li>
+<li>Step03 codice senza alcun riferimento al precedente copy_si_no</li>
+<li>Step04 eseguite tutte le sostituzioni nomi id e variabili con suffisso doppio underscore __</li>
+<li>Ricordarsi di dare un nome a setInterval per lo stop-and-go</li>
+<li>Si può tramite <q>fetch</q> di un file.txt inserire (insert) un file.css-like?</li>
+<li>Una simil-compilazione per snellire la lettura del codice</li>
+<li></li>
+
+</ol>
+</q>
+</blockquote>
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+`;
+
+
+
+
+document.getElementById("FORM_per_inserimento_nome_file__").innerHTML=
+`
+
+<section id="INSERISCE_il_nome_del_file_da_cercare__">
+ <input type="text" placeholder="Testo.txt" id="INPUT_NOME_FILE__" value="Testo.txt" >
+ 
+ <div id="INPUT_INSERITO__">
+ <!-- <div class="spinmin">&nbsp;</div> -->
+ <div class="spinner">&nbsp;</div>
+ Attendere...
+ </div>
+
+</section>
+
+
+`;
+
+
+
+function ChiudiHeader(){ 
+document.querySelector("#HEADER__").style.display="none"; 
+// 
+document.querySelector("#CHIUDI_HEADER__").style.display="none"; 
+document.querySelector("#APRI_HEADER__").style.display="block"; 
+
+};
+
+function ApriHeader(){ 
+document.querySelector("#HEADER__").style.display="block"; 
+// 
+document.querySelector("#CHIUDI_HEADER__").style.display="block"; 
+document.querySelector("#APRI_HEADER__").style.display="none"; 
+
+};
+
+
+
+function QuestionMarkPopup(){
+document.querySelector("#QUESTION_MARK_POPUP__").style.display="none";
+document.querySelector("#POPUP__").style.display="block";
+};
+
+function ChiudiQuestionMarkPopup(){
+document.querySelector("#QUESTION_MARK_POPUP__").style.display="block";
+document.querySelector("#POPUP__").style.display="none";
+};
+
+
+function COPY_SI(){ // alert("COPY_SI"); 
+clearInterval(EsegueIntervalPrendeNomeFileDaInput_00__);
+EsegueIntervalPrendeNomeFileDaInput_00__=null;
+document.querySelector("#COPY_SI__").style.display="none";
+document.querySelector("#COPY_NO__").style.display="block";
+}
+
+function COPY_NO(){ // alert("COPY_NO"); 
+//setInterval(PrendeNomeFileDaInput_00, 3000);
+EsegueIntervalPrendeNomeFileDaInput_00__ = setInterval(PrendeNomeFileDaInput_00, 3000);
+document.querySelector("#COPY_SI__").style.display="block";
+document.querySelector("#COPY_NO__").style.display="none";
+}
+
+
+
+
+</script>
+
+
+
+<main id="MAIN__"><div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">MAIN__ Attendere...</q></main>
+<script>
+document.getElementById("MAIN__").innerHTML=
+`
+
+<div id="VISUALIZZA_TESTO_00__"><div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">VISUALIZZA_TESTO_00__ Attendere...</q></div>
+
+`;
+
+</script>
+
+
+
+<footer id="FOOTER__"><div class="spinmin">&nbsp;</div><q style="font-size:0.75rem;">FOOTER__ Attendere...</q></footer>
+<script>
+document.getElementById("FOOTER__").innerHTML=
+`
+
+`;
+
+//
+// Testing per value responsive
+//
+//
+//
+function RangeDot_Dinamico_Input(){
+
+
+
+ValoreInizialeRange__=50;
+if(DESKTOP_CHECK==1){
+ValoreInizialeRange__=25.0;
+ValoreInizialeRange__=12.5;
+
+}
+
+
+
+ document.getElementById("LIV_01__").innerHTML=
+ `
+
+ <input id="RANGER__" type="range" value="`+ValoreInizialeRange__+`" step="1" min="1" max="99">
+
+ `;
+}; 
+RangeDot_Dinamico_Input() ;
+
+
+
+
+// Legge i valori del range
+// 
+// 
+
+function LeggiRangeDot_Dinamico_Input(){
+// Prende il valore dal range
+// 
+// 
+ let LetturaRange__ = document.getElementById("RANGER__").value;
+ FonteDinamica__ = (LetturaRange__ * 0.075) ;
+//
+//
+// 
+
+document.getElementById("VALORE_di_FonteDinamica__").innerHTML=
+ ( FonteDinamica__.toFixed(2) )+`rem;`;
+ 
+
+document.getElementById("SETTAGGI__").innerHTML=
+`
+<style>
+
+
+#CHECK_STOP_AND_GO__{font-size:`+(FonteDinamica__*0.75)+`rem;}
+#COMMENTI__{font-size:`+(FonteDinamica__*0.75)+`rem;}
+
+
+
+#INPUT_NOME_FILE__{
+width:75%;
+font-size:`+(FonteDinamica__ * 1)+`rem;
+font-size:2.5rem;
+
+border:solid 0.15rem lime;
+border:solid 0.00rem lime;
+
+}
+
+#INSERISCE_il_nome_del_file_da_cercare__{
+font-size:`+(FonteDinamica__ * 1)+`rem;
+font-size:1.25rem;
+}
+
+
+
+
+
+
+
+#VISUALIZZA_TESTO_00__{
+font-size:`+(FonteDinamica__ * 1)+`rem;overflow:auto!important;
+
+}
+
+
+
+</style>
+`;
+
+
+};
+// LeggiRangeDot_Dinamico_Input();
+setInterval(LeggiRangeDot_Dinamico_Input,1);
+
+
+
+
+// Inserimento dei dati (Testo.txt) tramite input
+//
+//
+function PrendeNomeFileDaInput_00(){
+
+	NomeDelFileDaInput__=document.getElementById("INPUT_NOME_FILE__").value;
+
+
+
+
+
+// Cambia colore ogni setInterval
+//
+//	
+        var LettereColoreRandom__ = '0123456789ABCDEF';
+        var CreaColoreRandom__ = '#';
+        var IColoreRandom__;
+        for (IColoreRandom__ = 0; IColoreRandom__ < 6; IColoreRandom__++ ) {
+            CreaColoreRandom__ = CreaColoreRandom__+LettereColoreRandom__[Math.round(Math.random() * 15)];
+        }
+        document.getElementById("COLORE_RANDOM__").innerHTML=
+		`
+		<style>
+		#PILLOLA_COPY__{background-color:`+CreaColoreRandom__+`;}
+		</style>
+		
+		${CreaColoreRandom__}
+		
+		`
+		;
+        
+
+
+
+
+
+
+if(NomeDelFileDaInput__!==""){	
+	document.getElementById("INPUT_INSERITO__").innerHTML=`File richiesto: `+`<b>`+NomeDelFileDaInput__+`</b>`;
+
+
+// Viene recuperato il file richiesto
+//
+//
+	LaSceltaInterattivaDinamica__ = NomeDelFileDaInput__;
+	let DirectoryDove__; DirectoryDove__="";
+	let File_00__; File_00__=LaSceltaInterattivaDinamica__;
+
+// Viene aggiornato il file richiesto
+//
+//
+	File_00__ = DirectoryDove__+File_00__ + "?"+(new Date().getTime()) + (Math.random()) ;
+
+// Il file richiesto viene passato
+//
+//
+	let Richiedo_00__ = new XMLHttpRequest(); 
+
+Richiedo_00__.open("GET",File_00__,false);
+Richiedo_00__.onreadystatechange = function(){
+let Trovato_00__ = Richiedo_00__.responseText;
+
+TestoTrovatoDaLeggere__ = Trovato_00__;
+
+document.getElementById("VISUALIZZA_TESTO_00__").innerHTML=`${TestoTrovatoDaLeggere__}`; 
+
+
+document.getElementById("CHECK_STOP_AND_GO__").innerHTML=`${File_00__}`; 
+
+
+
+
+
+} ; Richiedo_00__.send();
+
+
+
+	
+}else{document.getElementById("INPUT_INSERITO__").innerHTML=``};	
+	
+	
+
+};
+// PrendeNomeFileDaInput_00();
+
+// setInterval(PrendeNomeFileDaInput_00, 3000);
+
+EsegueIntervalPrendeNomeFileDaInput_00__ = setInterval(PrendeNomeFileDaInput_00, 3000);
+
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+<section>
+<div id="fine__"><div class="spinmin" style="display:inline-block">&nbsp;</div><q style="font-size:0.75rem;">VALORE_di_FonteDinamica</q> </div>
+</section>
+<script>
+document.getElementById("fine__").innerHTML=``;
+
+
+//
+// Collezione funzioni utili
+//
+//
+//
+
+    function ColoreEsadecimaleRandom() {
+        var LettereColoreRandom__ = '0123456789ABCDEF';
+        var CreaColoreRandom__ = '#';
+        var IColoreRandom__;
+        for (IColoreRandom__ = 0; IColoreRandom__ < 6; IColoreRandom__++ ) {
+            CreaColoreRandom__ = CreaColoreRandom__+LettereColoreRandom__[Math.round(Math.random() * 15)];
+        }
+        return CreaColoreRandom__;
+    }
+    
+    ApplicaColoreRandom__=ColoreEsadecimaleRandom();
+
+
+
+
+</script>
+
+
+
+<!-- Avvisa setInterval running ini -->
+<div id="COLORE_RANDOM__"></div>
+<script>
+RilevatoCodiceColoreRandom__=document.getElementById("COLORE_RANDOM__").textContent;
+</script>
+<!-- Avvisa setInterval running fin -->
+
+
+</body>
+</html>
