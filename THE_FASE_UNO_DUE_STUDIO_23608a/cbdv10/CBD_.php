167bc82541b1deb80e88165ff954d4959cb608df
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2023-07-01 08:11:12 +0200

    Add files via upload

---
+++ THE_FASE_UNO_DUE_STUDIO_23608a/cbdv10/CBD_.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html id="HTML__" lang="it">
+
+
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="AlterVista - Editor HTML">
+
+<title>
+
+
+
+$
+
+
+
+CBD_23624_0730a_V10.txt
+
+</title>
+
+<link rel="icon" type="image/png" href="https://it.altervista.org/images/favicon/favicon-16x16.png" sizes="16x16"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+</head>
+
+<body id="BODY__" style="border:solid 0.25rem #99AFE5;border:solid 0.25rem #47A3D1;border:solid 0.25rem #84CE4A;border:solid 0.25rem #C3EAD9;border:solid 0.25rem #9887AD;">
+
+
+
+<div id="APP_UNO_DUE__"><div class="spinmin">&nbsp;</div><q class="spinmincaveat1">Loading elemento ...</q><q class="spinmincaveat2">APP_UNO_DUE__</q></div>
+
+<!-- -->
+<? include("PhpCBDUnoDueV10.txt") ?>
+
+
+
+
+<q>CBD_23624_0730a_V10.txt</q>
+<div id="fine__"></div>
+</body>
+</html>
+
+
+
