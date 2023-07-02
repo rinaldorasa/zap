fa85297aa344bafe4a2fe9891fd8ebb80b95cde8
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2023-07-02 08:48:57 +0200

    Add files via upload

---
+++ THE_FASE_UNO_DUE_STUDIO_23608a/cbdv10updated/CBDB_.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html id="HTML__" lang="it">
+
+
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="AlterVista - Editor HTML">
+
+<title>
+
+
+??
+
+
+
+CBD_23625_0745a_V10.txt
+
+</title>
+
+<link rel="icon" type="image/png" href="https://it.altervista.org/images/favicon/favicon-16x16.png" sizes="16x16"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+<!-- -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+
+
+</head>
+
+<body id="BODY__" style="border:solid 0.25rem #99AFE5;border:solid 0.25rem #47A3D1;border:solid 0.25rem #84CE4A;border:solid 0.25rem #C3EAD9;border:solid 0.25rem #9887AD;">
+
+
+
+<div id="APP_UNO_DUE__"><div class="spinmin">&nbsp;</div><q class="spinmincaveat1">Loading elemento ...</q><q class="spinmincaveat2">APP_UNO_DUE__</q></div>
+
+<!-- -->
+<? include("PhpCBDUnoDueV10.txt") ?>
+
+
+
+
+<q>CBD_23625_0745a_V10.txt</q>
+<div id="fine__"></div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
